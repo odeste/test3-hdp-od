1513a43cba81ec1b43c589a9321cb37377a4ee7c
odeste <olivier.deste@wanadoo.fr>
2016-02-26 12:24:44 +0100

    Initial commit

---
+++ map-red-od/src/OdMapRed.java
@@ -0,0 +1,36 @@
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+/**
+ * Created by od on 2/26/2016.
+ */
+public class OdMapRed extends Mapper <LongWritable, Text, LongWritable, Text> {
+    public OdMapRed() {
+        super();
+    }
+
+    @Override
+    protected void setup(Context context) throws IOException, InterruptedException {
+        super.setup(context);
+    }
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        context.write(key, value);
+    }
+
+    @Override
+    protected void cleanup(Context context) throws IOException, InterruptedException {
+        super.cleanup(context);
+    }
+
+    @Override
+    public void run(Context context) throws IOException, InterruptedException {
+        super.run(context);
+    }
+
+}
